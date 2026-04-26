fdc0c3843d7e12a3bcc7b901739fe51ef2d129fa
Vedran Bojanić <vedran.bojanich@gmail.com>
2026-04-26 16:33:45 +0200

    WIP: Database information added. Failing test for happy scenario. We still need to implement saving to the DB.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("localhost:8000")?;
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("localhost:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
